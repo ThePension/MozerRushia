358fbfb2a5a149aaeea28baaaa7cd4bdd8ed7498
Kühner Jérémy <jeremy.kuhner@he-arc.ch>
2021-08-30 11:36:14 +0200

    Ajout d'une fonction suppression d'alien

--- MozerRushia/stage.h
+++ MozerRushia/stage.h
@@ -11,10 +11,12 @@ public:
 private:
     //std::vector<*Alien> aliens;
     int level = 1;
+    void removeAlien(Alien *a);
 
 public slots:
     void spawn(QGraphicsScene *scene);
     void moveAliens();
+    void increaseScore();
 };
 
 #endif // STAGE_H
